eb6f4dec69f19ed26c0521489f0e828708d8bea4
Neal DeBuhr <ndebuhr@gmail.com>
2022-08-14 01:44:10 +0000

    Introduce default firewall rules to facilitate use of the Isidro network for additional applications

--- provisioning/modules/foundation/network.tf
+++ provisioning/modules/foundation/network.tf
@@ -2,3 +2,55 @@ resource "google_compute_network" "isidro" {
   name                    = "isidro"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.isidro.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh-server"]
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "allow-http"
+  network = google_compute_network.isidro.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_firewall" "https" {
+  name    = "allow-https"
+  network = google_compute_network.isidro.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["https-server"]
+}
+
+resource "google_compute_firewall" "rdp" {
+  name    = "allow-rdp"
+  network = google_compute_network.isidro.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["rdp-server"]
+}
