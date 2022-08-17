bfc4c600814f39f46108a8fb1f9be7ec1d000bb6
Neal DeBuhr <ndebuhr@gmail.com>
2022-08-17 15:39:11 +0000

    Fix the parameterization of Cloud Memorystore in the Terraform foundation module

--- provisioning/modules/foundation/memorystore.tf
+++ provisioning/modules/foundation/memorystore.tf
@@ -1,6 +1,6 @@
 resource "google_redis_instance" "isidro" {
   name               = "isidro"
-  tier               = "BASIC"
+  tier               = var.memorystore_tier
   memory_size_gb     = 8
   region             = "us-central1"
   authorized_network = google_compute_network.isidro.id
