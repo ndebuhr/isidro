19ae9dba6829eb59c3ece965f34549a44cc3e6e7
Neal DeBuhr <ndebuhr@gmail.com>
2023-01-28 03:29:35 +0000

    Update the Intent service to use the latest model naming

--- intents/service/main.py
+++ intents/service/main.py
@@ -22,13 +22,12 @@ binarizer.fit_transform([
 ])
 
 classifier_model = tf.keras.models.load_model(
-    'intents.keras',
+    'model',
     custom_objects={'KerasLayer': hub.KerasLayer}
 )
 
 @app.route("/v1/intents", methods=["POST"])
 def repeat():
-    print(request.get_json())
     results = tf.nn.softmax(classifier_model(tf.constant(request.get_json())))
     intents = binarizer.inverse_transform(results.numpy())
     return json.dumps(list(intents))
